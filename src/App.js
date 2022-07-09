fbe5c52fbca1078ab576f211e04d9e7a2496fd20
grifsar <grifsar@gmail.com>
2022-07-08 19:41:57 -0600

    Updated styles and implemented random photo. Still need to get error handling in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-// import logo from './logo.svg';
 //6/21/2022 has a smaller than average photo
 //TODO Error handling - some days don't have an hdurl
 //TODO Error handling - handle user manually entering and searching for bad date
-//TODO random date function
 //TODO add photo function - may need to play with tailwind to get this looking good, if not manually style this
 //TODO play around with styling via Tailwind more
 
@@ -21,37 +19,47 @@ const App = () => {
   const searchPhotos = async (date) => {
     const response = await fetch(`${nasa}?date=${date}&api_key=${nasaApiKey}`);
     const data = await response.json();
+    //05/10/2010
+    console.log({data})
     setPhoto(data);
   }
   function formatDate4API(date) {
     return date.getFullYear() + '-' + (date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`) + '-' + (date.getDate() > 9 ? date.getDate() : '0' + date.getDate());
   }
+  function randomDate(){
+    let minDate = new Date('1995-06-20').getTime(); //first day with a photo of the day in milliseconds
+    const randDate = new Date(Math.random() * (today.getTime() - minDate) + minDate); //makes sure that date is in range
+    const formattedDate = formatDate4API(new Date(randDate));
+    setDate(formattedDate);
+    searchPhotos(formattedDate);
+  }
   const [photo, setPhoto] = useState('');
   const todayFormatted = formatDate4API(today);
   const [searchDate, setDate] = useState(todayFormatted);
-  // setDate('');
-  // searchDate = setDate(todayFormatted);
   useEffect(() => {
     searchPhotos(todayFormatted);
-  }, []); // the ,[] should make sure this runs once on load
+  }, [todayFormatted]); // the ,[] should make sure this runs once on load
   return (
     <div className="App container mx-auto py-4">
       <header className='text-center'>
-        <h1 className='text-4xl'>Nasa API Experiments</h1>
-        <h2 className='text-2xl'>Photo of the day</h2>
+        <h1 className='text-4xl font-semibold'>Nasa API Experiments</h1>
+        <h2 className='text-2xl font-semibold'>Photo of the day</h2>
       </header>
       <menu className='mt-6 text-center'>
         <label htmlFor="photoDay">
           <p>Pick a date between June 20th, 1995 and today to display the photo for that day</p>
         </label>
-        <input className='border-2 border-indigo-700 mt-6' type="date" name="photoDay" id="photoDay" min="1995-06-20" max={todayFormatted}
+        <input className='border-2 border-indigo-700 text-indigo-700 font-semibold mt-6' type="date" name="photoDay" id="photoDay" min="1995-06-20" max={todayFormatted}
           value={searchDate}
           onChange={(e) => setDate(e.target.value)}
         />
         <button
         className='px-4 py-2 font-semibold text-sm bg-indigo-700 text-white rounded-full shadow-sm ml-6'
-          onClick={() => searchPhotos(searchDate)}>Search</button><br />
-        {/* <button id="btn-randomPhoto">Get Random Photo</button> */}
+          onClick={() => searchPhotos(searchDate)}>Search</button>
+        <button 
+          className='px-4 py-2 font-semibold text-sm bg-indigo-700 text-white rounded-full shadow-sm ml-6'
+          onClick={() => randomDate()}
+          >Random</button>
       </menu>
       <main>
         <PhotoCard photo={photo} />
--- src/PhotoCard.jsx
+++ src/PhotoCard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-const PhotoCard = ({photo}) => {
+const PhotoCard = ({ photo }) => {
     return (
         <div className="container mx-auto mt-6">
-            <h3 className="text-xl text-center">{photo.title}</h3>
-            <img src={photo.hdurl} alt='' className="mx-auto mt-3"/>
-            <div className="mt-3">
-                {photo.explanation}
+            <div className="grid md:grid-cols-2 items-center flex-row-reverse">
+                {photo.media_type === 'image'}
+                <img src={photo.hdurl} alt='' className="mx-auto mt-3 order-2 md:order-1" />
+                <div className="m-3 order-1 md:order2">
+                    <h3 className="text-xl text-center font-semibold">{photo.title ? photo.title : 'Untitled'}</h3>
+                    {photo.explanation}
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
 export default PhotoCard;
