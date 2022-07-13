ecde9b62347bc1992581bdb5aa61b58da40e5c8f
grifsar <grifsar@gmail.com>
2022-07-13 15:07:05 -0600

    Uploading style updates etc.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 //6/21/2022 has a smaller than average photo
-//TODO Error handling - some days don't have an hdurl
-//TODO Error handling - handle user manually entering and searching for bad date
 //TODO add photo function - may need to play with tailwind to get this looking good, if not manually style this
 //TODO play around with styling via Tailwind more
+//"homepage": "/react-nasaphoto",
 
 import './App.css';
 import React, { useEffect, useState } from 'react';
+
 import PhotoCard from './PhotoCard';
 
 const today = new Date();
@@ -20,7 +20,7 @@ const App = () => {
     const response = await fetch(`${nasa}?date=${date}&api_key=${nasaApiKey}`);
     const data = await response.json();
     //05/10/2010
-    console.log({data})
+    // console.log({data})
     setPhoto(data);
   }
   function formatDate4API(date) {
@@ -40,30 +40,31 @@ const App = () => {
     searchPhotos(todayFormatted);
   }, [todayFormatted]); // the ,[] should make sure this runs once on load
   return (
-    <div className="App container mx-auto py-4">
-      <header className='text-center'>
-        <h1 className='text-4xl font-semibold'>Nasa API Experiments</h1>
-        <h2 className='text-2xl font-semibold'>Photo of the day</h2>
-      </header>
-      <menu className='mt-6 text-center'>
-        <label htmlFor="photoDay">
-          <p>Pick a date between June 20th, 1995 and today to display the photo for that day</p>
-        </label>
-        <input className='border-2 border-indigo-700 text-indigo-700 font-semibold mt-6' type="date" name="photoDay" id="photoDay" min="1995-06-20" max={todayFormatted}
-          value={searchDate}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <button
-        className='px-4 py-2 font-semibold text-sm bg-indigo-700 text-white rounded-full shadow-sm ml-6'
-          onClick={() => searchPhotos(searchDate)}>Search</button>
-        <button 
+    <div className="App container mx-auto pt-4 bg-white">
+     
+        <header className='text-center'>
+          <h1 className='text-4xl font-semibold'>Nasa API Experiments</h1>
+          <h2 className='text-2xl font-semibold'>Photo of the day</h2>
+        </header>
+        <menu className='mt-6 text-center'>
+          <label htmlFor="photoDay">
+            <p>Pick a date between June 20th, 1995 and today to display the photo for that day</p>
+          </label>
+          <input className='border-2 border-indigo-700 text-indigo-700 font-semibold mt-6' type="date" name="photoDay" id="photoDay" min="1995-06-20" max={todayFormatted}
+            value={searchDate}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <button
           className='px-4 py-2 font-semibold text-sm bg-indigo-700 text-white rounded-full shadow-sm ml-6'
-          onClick={() => randomDate()}
-          >Random</button>
-      </menu>
-      <main>
-        <PhotoCard photo={photo} />
-      </main>
+            onClick={() => searchPhotos(searchDate)}>Search</button>
+          <button
+            className='px-4 py-2 font-semibold text-sm bg-indigo-700 text-white rounded-full shadow-sm ml-6'
+            onClick={() => randomDate()}
+            >Random</button>
+        </menu>
+        <main>
+          <PhotoCard photo={photo} />
+        </main>
     </div>
   );
 }
--- src/PhotoCard.jsx
+++ src/PhotoCard.jsx
@@ -3,14 +3,25 @@ import React from "react";
 const PhotoCard = ({ photo }) => {
     return (
         <div className="container mx-auto mt-6">
-            <div className="grid md:grid-cols-2 items-center flex-row-reverse">
-                {photo.media_type === 'image'}
-                <img src={photo.hdurl} alt='' className="mx-auto mt-3 order-2 md:order-1" />
-                <div className="m-3 order-1 md:order2">
-                    <h3 className="text-xl text-center font-semibold">{photo.title ? photo.title : 'Untitled'}</h3>
-                    {photo.explanation}
+            {photo.code != null &&
+                <div>
+                    <h3 className="text-red-700 text-center">Something went wrong. Please make sure the date you entered is a date between <strong>June 20th, 1995 and today</strong></h3>
                 </div>
-            </div>
+            }
+            {photo.media_type === 'image' &&
+                <div className="grid md:grid-cols-2 items-center flex-row-reverse">
+                    <div className="flex justify-items-end"><img src={photo.hdurl} alt='' className="mx-auto mt-3 order-2 md:order-1" /></div>
+                    <div className="m-3 order-1 md:order2">
+                        <h3 className="text-xl text-center font-semibold">{photo.title ? photo.title : 'Untitled'}</h3>
+                        {photo.explanation}
+                    </div>
+                </div>
+            }
+            {photo.media_type !== 'image' && photo.code === null &&
+                <div>
+                    <h3 className="text-center">Today's photo of the day isn't actually an image</h3>
+                </div>
+            }
         </div >
     );
 }
