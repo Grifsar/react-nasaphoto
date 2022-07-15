c0b8e6c67966be614b2b07cb015955c4331be100
grifsar <grifsar@gmail.com>
2022-07-15 11:23:29 -0600

    Put this back here

---
+++ netlify/functions/nasaPhotoKey.js
@@ -0,0 +1,22 @@
+// const axios = require('axios');
+import fetch from "node-fetch";
+
+exports.handler = async function(event, context){
+    console.log('entered function');
+    console.log({event});
+    console.log({context});
+    try {
+        const { date } = event.queryStringParameters;
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_NASA_API}/`);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(response.data),
+        };
+      } catch (err) {
+        return {
+          statusCode: 404,
+          body: err.toString(),
+        };
+      }
+
+};
