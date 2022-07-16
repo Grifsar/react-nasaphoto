35159261a09e5a55284fab62805044cd140d4ca9
grifsar <grifsar@gmail.com>
2022-07-16 15:31:21 -0600

    Reverted a change

--- netlify/functions/nasaPhotoKey.js
+++ netlify/functions/nasaPhotoKey.js
@@ -9,20 +9,20 @@ exports.handler = async function (event, context) {
   try {
     const { date } = event.queryStringParameters;
     const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_NASA_API}/`);
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(response.data),
-      headers: {
-        "access-control-allow-origin": "*",
-      },
-    });
-    // return {
+    // callback(null, {
     //   statusCode: 200,
     //   body: JSON.stringify(response.data),
     //   headers: {
     //     "access-control-allow-origin": "*",
     //   },
-    // };
+    // });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+      headers: {
+        "access-control-allow-origin": "*",
+      },
+    };
   }
 
   catch (err) {
