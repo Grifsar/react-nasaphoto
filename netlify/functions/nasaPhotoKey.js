eaad9a5ee4317dda0916c1bffae994b3579e2222
grifsar <grifsar@gmail.com>
2022-07-16 15:19:19 -0600

    Trying this again...

--- netlify/functions/nasaPhotoKey.js
+++ netlify/functions/nasaPhotoKey.js
@@ -2,36 +2,36 @@
 // import fetch from "node-fetch";
 const fetch = require("node-fetch");
 
-exports.handler = async function(event, context){
-    console.log('entered function');
-    console.log({event});
-    console.log({context});
-    try {
-        const { date } = event.queryStringParameters;
-        const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_NASA_API}/`);
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(response.data),
-          headers: {
-            "access-control-allow-origin": "*",
-          },
-        });
-        // return {
-        //   statusCode: 200,
-        //   body: JSON.stringify(response.data),
-        //   headers: {
-        //     "access-control-allow-origin": "*",
-        //   },
-        // };
-      } 
-      
-      catch (err) {
-        return {
-          statusCode: 404,
-          body: err.toString(),
-          headers: {
-            "access-control-allow-origin": "*",
-          },
-        };
-      }
+exports.handler = async function (event, context) {
+  console.log('entered function');
+  console.log({ event });
+  console.log({ context });
+  try {
+    const { date } = event.queryStringParameters;
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_NASA_API}/`);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+      headers: {
+        "access-control-allow-origin": "*",
+      },
+    });
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify(response.data),
+    //   headers: {
+    //     "access-control-allow-origin": "*",
+    //   },
+    // };
+  }
+
+  catch (err) {
+    return {
+      statusCode: 404,
+      body: err.toString(),
+      headers: {
+        "access-control-allow-origin": "*",
+      },
+    };
+  }
 };
