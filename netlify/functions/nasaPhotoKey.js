6bf4743080f0432750b81c2fe29c84079f3e243b
grifsar <grifsar@gmail.com>
2022-07-16 15:45:58 -0600

    trying another toml setting

--- netlify/functions/nasaPhotoKey.js
+++ netlify/functions/nasaPhotoKey.js
@@ -8,7 +8,7 @@ exports.handler = async function (event, context) {
   console.log({ context });
   try {
     const { date } = event.queryStringParameters;
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_NASA_API}/`);
+    const response = await fetch(`/api/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_NASA_API}/`);
     // callback(null, {
     //   statusCode: 200,
     //   body: JSON.stringify(response.data),
