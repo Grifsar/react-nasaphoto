741a7a53fd148f5ccbeca2e84e13c5e6663b73bd
grifsar <grifsar@gmail.com>
2022-07-17 18:05:08 -0600

    My folder was named incorrectly, let's see if this helps

---
+++ .netlify/functions/nasaPhotoKey.js
@@ -0,0 +1,38 @@
+// const axios = require('axios');
+import fetch from "node-fetch";
+// const fetch = require("node-fetch");
+
+exports.handler = async function (event, context) {
+  console.log('entered function');
+  console.log({ event });
+  console.log({ context });
+  const { date } = event.queryStringParameters.date;
+  
+  try { 
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_NASA_API}/`);
+    // callback(null, {
+    //   statusCode: 200,
+    //   body: JSON.stringify(response.data),
+    //   headers: {
+    //     "access-control-allow-origin": "*",
+    //   },
+    // });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+      // headers: {
+      //   "access-control-allow-origin": "*",
+      // },
+    };
+  }
+
+  catch (err) {
+    return {
+      statusCode: 404,
+      body: err.toString(),
+      // headers: {
+      //   "access-control-allow-origin": "*",
+      // },
+    };
+  }
+};
