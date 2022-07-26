d87384362181d412aba3b98b92c0c56727a4d213
grifsar <grifsar@gmail.com>
2022-07-26 16:50:54 -0600

    Switched to params in url for get request to hopefully remove trailing slash...

--- .netlify/functions/nasaPhotoKey.js
+++ .netlify/functions/nasaPhotoKey.js
@@ -3,9 +3,13 @@ const axios = require('axios');
 exports.handler = async (event, context) => {
   const date = event.queryStringParameters.date;
   const apikay = process.env.REACT_APP_NASA_API;
-  const url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apikay}`;
+  const params = new URLSearchParams([
+    ['date', `${date}`],
+    ['api_key', `${apikay}`]
+]);
+  const url = 'https://api.nasa.gov/planetary/apod';
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {params});
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)
