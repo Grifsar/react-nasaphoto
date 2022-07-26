ae87dfde0f28078e5a766026c4028c0932080f2d
grifsar <grifsar@gmail.com>
2022-07-26 16:15:52 -0600

    Fixed the url in the function

--- .netlify/functions/nasaPhotoKey.js
+++ .netlify/functions/nasaPhotoKey.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.handler = async (event, context) => {
   const date = event.queryStringParameters.date;
   const apikay = process.env.REACT_APP_NASA_API;
-  const url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apikay}/`;
+  const url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apikay}`;
   try {
     const response = await axios.get(url);
     return {
