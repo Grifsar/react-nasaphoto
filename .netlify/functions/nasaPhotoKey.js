7cf187588586a4706c43659ab993ae71665ef979
grifsar <grifsar@gmail.com>
2022-07-27 14:48:46 -0600

    Switched to using fetch to see if that fixes the trailing slash issue in the api call

--- .netlify/functions/nasaPhotoKey.js
+++ .netlify/functions/nasaPhotoKey.js
@@ -1,24 +1,47 @@
+// const { resolvePath } = require("react-router-dom");
+const fetch = require('node-fetch');
+const url = 'https://api.nasa.gov/planetary/apod';
+const apikey = process.env.REACT_APP_NASA_API;
 
-const axios = require('axios');
+// const axios = require('axios');
 exports.handler = async (event, context) => {
   const date = event.queryStringParameters.date;
-  const apikay = process.env.REACT_APP_NASA_API;
-  const params = new URLSearchParams([
-    ['date', `${date}`],
-    ['api_key', `${apikay}`]
-]);
-  const url = 'https://api.nasa.gov/planetary/apod';
+  let response;
   try {
-    const response = await axios.get(url, {params});
+    response = await fetch(`${url}?date=${date}&api_key=${apikey}`)
+    // handle response
+  } catch (err) {
     return {
-      statusCode: 200,
-      body: JSON.stringify(response.data)
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
     }
   }
-  catch(error){
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error)
-    }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
   }
+
+//   const params = new URLSearchParams([
+//     ['date', `${date}`],
+//     ['api_key', `${apikay}`]
+// ]);
+//   
+//   try {
+//     const response = await axios.get(url, {params});
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(response.data)
+//     }
+//   }
+//   catch(error){
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify(error)
+//     }
+//   }
 };
