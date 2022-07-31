5a4e78c675b66b4c709198bb7cd5d161cf3d7eab
grifsar <grifsar@gmail.com>
2022-07-31 12:10:33 -0600

    Seeing if this is where the code is supposed to go from the post

--- .netlify/functions/nasaPhotoKey.js
+++ .netlify/functions/nasaPhotoKey.js
@@ -1,19 +1,24 @@
 // const { resolvePath } = require("react-router-dom");
 // const fetch = require('node-fetch');
-const url = 'https://api.nasa.gov/planetary/apod';
-const apikey = process.env.REACT_APP_NASA_API;
+// const url = 'https://api.nasa.gov/planetary/apod';
+// const apikey = process.env.REACT_APP_NASA_API;
 
 const axios = require('axios');
 exports.handler = async (event, context) => {
-  const date = event.queryStringParameters.date;
-  let response;
-  const params = new URLSearchParams([
-    ['date', `${date}`],
-    ['api_key', `${apikey}`]
-]);
+  // const date = event.queryStringParameters.date;
+  // const params = new URLSearchParams([
+  //   ['date', `${date}`],
+  //   ['api_key', `${apikey}`]
+  // ]);
   // const url = 'https://api.nasa.gov/planetary/apod';
   try {
-    const response = await axios.get(url, {params});
+    const response = await axios.get({
+      params: {
+        apikey: process.env.REACT_APP_NASA_API,
+        date: event.queryStringParameters.date
+      },
+      url: 'https://api.nasa.gov/planetary/apod'
+    });
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)
@@ -27,22 +32,22 @@ exports.handler = async (event, context) => {
     }
   }
 
-//   const params = new URLSearchParams([
-//     ['date', `${date}`],
-//     ['api_key', `${apikay}`]
-// ]);
-//   
-//   try {
-//     const response = await axios.get(url, {params});
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(response.data)
-//     }
-//   }
-//   catch(error){
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify(error)
-//     }
-//   }
+  //   const params = new URLSearchParams([
+  //     ['date', `${date}`],
+  //     ['api_key', `${apikay}`]
+  // ]);
+  //   
+  //   try {
+  //     const response = await axios.get(url, {params});
+  //     return {
+  //       statusCode: 200,
+  //       body: JSON.stringify(response.data)
+  //     }
+  //   }
+  //   catch(error){
+  //     return {
+  //       statusCode: 500,
+  //       body: JSON.stringify(error)
+  //     }
+  //   }
 };
