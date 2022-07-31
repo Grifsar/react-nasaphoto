06a322e849a9f8e7bdce1ce924f0021b0fb94ae9
grifsar <grifsar@gmail.com>
2022-07-31 11:13:01 -0600

    Trying axios again at the recommendation of a response to my post

--- .netlify/functions/nasaPhotoKey.js
+++ .netlify/functions/nasaPhotoKey.js
@@ -3,13 +3,21 @@
 const url = 'https://api.nasa.gov/planetary/apod';
 const apikey = process.env.REACT_APP_NASA_API;
 
-// const axios = require('axios');
+const axios = require('axios');
 exports.handler = async (event, context) => {
   const date = event.queryStringParameters.date;
   let response;
+  const params = new URLSearchParams([
+    ['date', `${date}`],
+    ['api_key', `${apikey}`]
+]);
+  // const url = 'https://api.nasa.gov/planetary/apod';
   try {
-    response = await fetch(`${url}?date=${date}&api_key=${apikey}`)
-    // handle response
+    const response = await axios.get(url, {params});
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    }
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
@@ -17,13 +25,6 @@ exports.handler = async (event, context) => {
         error: err.message
       })
     }
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: response
-    })
   }
 
 //   const params = new URLSearchParams([
